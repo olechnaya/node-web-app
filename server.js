eb3258a249819071775105a4f5b619a547b8f694
Olga <data19101988@gmail.com>
2018-11-03 20:11:47 +0300

    All other  changes

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const hbs = require('hbs');
+var hbsutils = require('hbs-utils')(hbs);
+const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
+hbsutils.registerWatchedPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req,res) => {
@@ -24,6 +39,6 @@ app.get('/about', (req,res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
